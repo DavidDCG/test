f97d4a4bac960d75e788248557b9d207b471205d
DavidDCG <110427142+DavidDCG@users.noreply.github.com>
2024-08-02 16:13:09 -0600

    yy
    yy

--- src/controller/configuracionController.js
+++ src/controller/configuracionController.js
@@ -1,71 +1,81 @@
 const { removeBackground } = require('@imgly/background-removal-node');
-const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const { bucket } = require('../../config/firebase-config');
-const path = require('path');
 const Validator = require('validatorjs');
 
-async function removeImageBackground(req = request, res = response) {
-    try {
-        const ruta_destino_firebase = "Halconet/Firmas/";
-        const guid = uuidv4();
-        
-        // Definir las reglas de validación
-        const rules = {
-            url_img: 'required|string'
-        };
-        
-        // Crear una nueva instancia de Validator
-        const validation = new Validator(req.body, rules);
-        if (validation.fails()) {
-            throw new Error('Validation Error: ' + JSON.stringify(validation.errors.all()));
-        }
+function removeImageBackground(req, res) {
+    console.log("1. Definición de variables");
+    const ruta_destino_firebase = "Halconet/Firmas/";
+    const guid = uuidv4();
+    console.log("2. Definición de reglas");
 
-        const url_get = req.body.url_img;
+    // Definir las reglas de validación
+    const rules = {
+        url_img: 'required|string'
+    };
+    console.log("3. Revisión de reglas");
 
-        // Establecer un timeout para la operación de eliminación de fondo
-        const timeoutPromise = new Promise((_, reject) => {
-            setTimeout(() => reject(new Error('Timeout: La operación tomó demasiado tiempo')), 30000); // 30 segundos
-        });
-
-        // Ejecutar la operación de eliminación de fondo con timeout
-        const blob = await Promise.race([removeBackground(url_get), timeoutPromise]);
-        
-        const buffer = Buffer.from(await blob.arrayBuffer());
-        
-        // Referencia al archivo en Firebase Storage
-        const file = bucket.file(ruta_destino_firebase + guid + ".png");
-        await file.save(buffer, {
-            metadata: {
-                contentType: "image/png",
-            },
-        });
-
-        // Obtener la URL pública con el token de acceso
-        const [publicUrl] = await file.getSignedUrl({
-            action: 'read',
-            expires: '03-01-2500'
-        });
-
-        res.json({
-            valid: true,
-            message: "Transacción válida, imagen subida exitosamente a",
-            data: {
-                nombre: guid + ".png",
-                ruta_publica: publicUrl,
-                ruta_firebase: ruta_destino_firebase
-            }
-        });
-
-    } catch (error) {
-        console.error('Error:', error); // Registrar el error
-        res.status(400).json({
+    // Crear una nueva instancia de Validator
+    const validation = new Validator(req.body, rules);
+    if (validation.fails()) {
+        return res.status(400).json({
             valid: false,
-            message: "Transacción no válida",
-            data: { error: error.message }
+            message: 'Validation Error: ' + JSON.stringify(validation.errors.all())
         });
     }
+
+    const url_get = req.body.url_img;
+    console.log("4. Procesamiento de la imagen");
+
+    // Ejecutar la operación de eliminación de fondo
+    removeBackground(url_get)
+        .then(blob => {
+            console.log("5. Conversión de blob de imagen a array");
+
+            // Convertir el blob de imagen a buffer
+            return blob.arrayBuffer().then(arrayBuffer => Buffer.from(arrayBuffer));
+        })
+        .then(buffer => {
+            console.log("6. Almacenamiento en Firebase");
+
+            // Referencia al archivo en Firebase Storage
+            const file = bucket.file(ruta_destino_firebase + guid + ".png");
+
+            // Guardar el buffer en Firebase Storage
+            return file.save(buffer, {
+                metadata: {
+                    contentType: "image/png"
+                }
+            }).then(() => {
+                // Obtener la URL pública
+                return file.getSignedUrl({
+                    action: 'read',
+                    expires: '03-01-2500'
+                });
+            });
+        })
+        .then(([publicUrl]) => {
+            console.log("7. Generación de URL para lectura");
+
+            console.log("8. JSON final resultado ");
+            res.json({
+                valid: true,
+                message: "Transacción válida, imagen subida exitosamente a",
+                data: {
+                    nombre: guid + ".png",
+                    ruta_publica: publicUrl,
+                    ruta_firebase: ruta_destino_firebase
+                }
+            });
+        })
+        .catch(error => {
+            console.error('9. Generado Error:', error); // Registrar el error
+            res.status(400).json({
+                valid: false,
+                message: "Transacción no válida",
+                data: { error: error.message }
+            });
+        });
 }
 
 module.exports = { removeImageBackground };
-
